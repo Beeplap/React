ece1fcdbcbada10ec960e73ed7e3cb29a2fe0c41
Beeplap <ghartibeeplap@gmail.com>
2025-04-29 09:28:15 +0545

    maintained hero section,applied proper css to beautify, added new button and converted it to my portfolio

--- day2/src/Hero.jsx
+++ day2/src/Hero.jsx
@@ -3,14 +3,21 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-700 text-white">
-      <h1 className="text-4xl font-mono mb-4">Welcome to ArchWiki</h1>
-      <p className="text-lg mb-8 font-mono">
-        Your one-stop solution for all things Arch Linux.
+    <div className="font-mono flex flex-col items-center justify-center h-screen bg-gray-700 text-white ">
+      <h1 className="text-4xl  mb-4">Welcome to my Profile</h1>
+      <p className=" text-lg mb-8 max-w-xl">
+        I am a passionate developer with a love for creating beautiful and
+        functional web applications. Let's work together to bring your ideas to
+        life!
       </p>
-      <button className="bg-cyan-600 font-mono text-white px-4 py-2 rounded hover:bg-cyan-500 transition duration-300">
-        Get Started
+      <div className="flex gap-4">
+      <button className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition duration-300 ">
+        View My Work
       </button>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300 ">
+        Contact Me
+      </button>
+      </div>
     </div>
   );
 };
--- day2/src/Navbar.jsx
+++ day2/src/Navbar.jsx
@@ -7,8 +7,8 @@ import { IoMdContacts } from "react-icons/io";
 const Navbar = () => {
   return (
     <nav className="font-mono flex justify-between items-center  bg-gray-800 text-gray-300 p-4">
-      <div className="font-black"> Portfolio</div>
-      <ul className="  flex items-center justifycont-center gap-6 ">
+      <div className="font-black text-2xl"> Portfolio</div>
+      <ul className=" font-bold flex items-center justifycont-center gap-6 ">
         <li className="flex items-center gap-2 hover:text-white cursor-pointer hover:scale-105 duration-300 hover:bg-gray-700 p-2 rounded-md ">
           {" "}
           <TiHome /> Home
